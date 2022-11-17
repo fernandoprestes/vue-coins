dec799e1b3bc56a0a206908462022d133d40fd21
fernandoprestes <fernandoprestes@users.noreply.github.com>
2022-11-17 17:12:49 +0000

    deploy: 9f09985e29894ba4b53e25ee5ad953017d0be5ce

--- assets/polkadot.58caedfe.js
+++ assets/polkadot.58caedfe.js
@@ -1,1 +1,1 @@
-const a="/vue-coins /assets/polkadot.ba662d9a.png";export{a as default};
+const a="/vue-coins/assets/polkadot.ba662d9a.png";export{a as default};
--- assets/solana.d1b2fbdc.js
+++ assets/solana.d1b2fbdc.js
@@ -1,1 +1,1 @@
-const s="/vue-coins /assets/solana.e5721ef7.png";export{s as default};
+const s="/vue-coins/assets/solana.e5721ef7.png";export{s as default};
